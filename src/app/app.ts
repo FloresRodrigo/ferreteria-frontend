b28b88d610c7e35aab9a87538d3ff684c53261d1
FloresRodrigo <rodrigoef25@gmail.com>
2026-02-23 22:46:34 -0300

    Chatbot integrado
    Se integró un chatbot en la pagina con respuestas predefinidas y posibilidad de enviar un email a en caso de necesitarlo, con esto se da por finalizado el desarrollo del a pagina web

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 import { CommonModule } from '@angular/common';
+import { Chatbot } from './components/chatbot/chatbot';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, CommonModule],
+  imports: [RouterOutlet, Header, Footer, CommonModule, Chatbot],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
