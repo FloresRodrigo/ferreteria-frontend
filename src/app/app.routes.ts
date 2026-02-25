ce8be0d543b21633a7c9cc97fcb55556c7eb8eae
FloresRodrigo <rodrigoef25@gmail.com>
2026-02-24 21:19:33 -0300

    Remover recuperar contraseña
    Se remueven los componentes que permitían la recuperación de contraseña, también se elimina el olvide mi contraseña del formulario de login. Se agrega un comodín en las rutas para que siempre lleve al home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Registro } from './components/registro/registro';
 import { Login } from './components/login/login';
-import { ForgotPassword } from './components/forgot-password/forgot-password';
-import { ResetPassword } from './components/reset-password/reset-password';
+//import { ForgotPassword } from './components/forgot-password/forgot-password';
+//import { ResetPassword } from './components/reset-password/reset-password';
 import { AuthGuard } from './services/auth-guard';
 import { GuestGuard } from './services/guest-guard';
 import { AdminGuard } from './services/admin-guard';
@@ -18,10 +18,11 @@ import { SetPassword } from './components/set-password/set-password';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: '**', redirectTo: ''},
   { path: 'registro', canActivate: [GuestGuard], component: Registro },
   { path: 'login', canActivate: [GuestGuard], component: Login },
-  { path: 'forgot-password', canActivate: [GuestGuard], component: ForgotPassword },
-  { path: 'reset-password', canActivate: [GuestGuard], component: ResetPassword },
+  //{ path: 'forgot-password', canActivate: [GuestGuard], component: ForgotPassword },
+  //{ path: 'reset-password', canActivate: [GuestGuard], component: ResetPassword },
   { path: 'inventario', canActivate: [AdminGuard], component: Inventario },
   { path: 'inventario-form', canActivate: [AdminGuard], component: InventarioForm },
   { path: 'inventario-form/:id', canActivate: [AdminGuard], component: InventarioForm },
